c20561699b7f112a8df924cc2b15658331d2b7af
shafie-isak <shafiewisak@gmail.com>
2025-06-16 17:47:40 +0300

    littel change on server start

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use((err, req, res, next) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 9000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
